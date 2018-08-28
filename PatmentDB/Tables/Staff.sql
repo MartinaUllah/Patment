2c985110aa0133b61a9986f59ab61ac3304060dc
Martina <Martina@DESKTOP-9D53C89>
2018-08-28 20:48:00 +0100

    Adding Staff and StaffType tables

---
+++ PatmentDB/Tables/Staff.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Staff]
+(
+	[StaffID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[FirstName] [varchar](150) NOT NULL,
+	[LastName] [varchar](150) NOT NULL,
+	[StartDate] [date] NOT NULL,
+	[DateOfBirth] [date] NOT NULL,
+	[StaffTypeID] [int] NOT NULL, 
+	CONSTRAINT Fk_StaffType_Staff Foreign Key (StaffTypeID) References StaffType(StaffTypeID)
+)
+
+
---
+++ PatmentDB/Tables/StaffType.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[StaffType]
+(
+	[StaffTypeID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[StaffType] [varchar](150) NOT NULL,
+)
